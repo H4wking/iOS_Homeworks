2a6b0fcbd122c4ac04e3d1098a59bdca9d230d5d
H4wking <nazaruk@ucu.edu.ua>
2020-11-08 22:51:38 +0200

    HW2

---
+++ HW2/Notes/Notes/Note.swift
@@ -0,0 +1,9 @@
+//
+//  Note.swift
+//  Notes
+//
+//  Created by Danylo Nazaruk on 07.11.2020.
+//  Copyright © 2020 Danylo Nazaruk. All rights reserved.
+//
+
+import Foundation
---
+++ HW2/Notes/Notes/NoteDataManager.swift
@@ -0,0 +1,9 @@
+//
+//  NoteDataManager.swift
+//  Notes
+//
+//  Created by Danylo Nazaruk on 07.11.2020.
+//  Copyright © 2020 Danylo Nazaruk. All rights reserved.
+//
+
+import Foundation
