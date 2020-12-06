940d70ac73ea8e669bf6342e0cec064f18eef422
H4wking <nazaruk@ucu.edu.ua>
2020-12-06 23:13:35 +0200

    HW3

---
+++ HW3/Notes/Notes/CoreDataStorage.swift
@@ -0,0 +1,9 @@
+//
+//  CoreDataStorage.swift
+//  Notes
+//
+//  Created by Danylo Nazaruk on 06.12.2020.
+//  Copyright © 2020 Danylo Nazaruk. All rights reserved.
+//
+
+import Foundation
---
+++ HW3/Notes/Notes/Persistence.swift
@@ -0,0 +1,9 @@
+//
+//  Persistence.swift
+//  Notes
+//
+//  Created by Danylo Nazaruk on 06.12.2020.
+//  Copyright © 2020 Danylo Nazaruk. All rights reserved.
+//
+
+import Foundation
